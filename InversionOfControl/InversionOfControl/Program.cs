37939bc35bbec32574975d592a879ee7d9025ee5
sharlin <send2sharlin@gmail.com>
2017-05-28 14:43:13 +0530

    Code implemented.

--- InversionOfControl/InversionOfControl/Program.cs
+++ InversionOfControl/InversionOfControl/Program.cs
@@ -10,6 +10,52 @@ namespace InversionOfControl
     {
         static void Main(string[] args)
         {
+            User user = new User(new MySQLDal());
+            user.Add();
+        }
+    }
+
+    public class User
+    {
+        IDal dal;
+
+        public User(IDal dalService)
+        {
+            dal = dalService;
+        }
+        
+        public bool IsValid
+        {
+            get { return true; }
+        }
+
+        public void Add()
+        {
+            if(IsValid)
+            {
+                dal.Add(this);
+            }            
+        }
+    }
+
+    public interface IDal
+    {
+        void Add(User user);
+    }
+
+    public class MySQLDal : IDal
+    {
+        public void Add(User user)
+        {
+            //Here your code
+        }
+    }
+
+    public class MySqlServerDal : IDal
+    {
+        public void Add(User user)
+        {
+            //Here your code
         }
     }
 }
